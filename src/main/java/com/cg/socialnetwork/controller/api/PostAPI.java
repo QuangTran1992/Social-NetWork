aafd2f20df84ffc1672a5fa5f2dbfc3afc5e645d
DELL <nphuoc0402@gmail.com>
2021-10-21 14:11:55 +0700

    create post

--- src/main/java/com/cg/socialnetwork/controller/api/PostAPI.java
+++ src/main/java/com/cg/socialnetwork/controller/api/PostAPI.java
@@ -7,6 +7,8 @@ import com.cg.socialnetwork.service.PostService.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -52,6 +54,9 @@ public class PostAPI {
         }
     }
 
-//    @PostMapping
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post){
+        return new ResponseEntity<>(postService.save(post),HttpStatus.CREATED);
+    }
 
 }
